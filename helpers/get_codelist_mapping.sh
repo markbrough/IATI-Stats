861b6fa4478035b095ad69f73180f119c4a51c68
Andy Lulham <a.lulham@gmail.com>
2021-03-28 23:14:11 +0100

    Switch to use JSON mappings

--- helpers/get_codelist_mapping.sh
+++ helpers/get_codelist_mapping.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 wget https://raw.github.com/IATI/IATI-Codelists/version-1.05/mapping.xml -O mapping-1.xml
+python mappings_to_json.py mapping-1.xml > mapping-1.json
+
 wget https://raw.github.com/andylolz/IATI-Codelists/version-2.03/mapping.xml -O mapping-2.xml
+python mappings_to_json.py mapping-2.xml > mapping-2.json
