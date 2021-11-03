c468e4f1ed85b1d12e303649c7f864e763434d29
Andy Lulham <a.lulham@gmail.com>
2021-11-03 20:02:58 +0000

    Fetch v1 codelists from the codeforIATI API

--- helpers/get_codelists.sh
+++ helpers/get_codelists.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 mkdir -p codelists/1
-wget "https://iatistandard.org/reference_downloads/105/codelists/downloads/clv2/json/en/Version.json" -O codelists/1/Version.json
-wget "https://iatistandard.org/reference_downloads/105/codelists/downloads/clv2/json/en/ActivityStatus.json" -O codelists/1/ActivityStatus.json
-wget "https://iatistandard.org/reference_downloads/105/codelists/downloads/clv2/json/en/Currency.json" -O codelists/1/Currency.json
-wget "https://iatistandard.org/reference_downloads/105/codelists/downloads/clv2/json/en/Sector.json" -O codelists/1/Sector.json
-wget "https://iatistandard.org/reference_downloads/105/codelists/downloads/clv2/json/en/SectorCategory.json" -O codelists/1/SectorCategory.json
-wget "https://iatistandard.org/reference_downloads/105/codelists/downloads/clv2/json/en/DocumentCategory.json" -O codelists/1/DocumentCategory.json
-wget "https://iatistandard.org/reference_downloads/105/codelists/downloads/clv2/json/en/AidType.json" -O codelists/1/AidType.json
-wget "https://iatistandard.org/reference_downloads/105/codelists/downloads/clv2/json/en/BudgetNotProvided.json" -O codelists/1/BudgetNotProvided.json
+wget "https://codelists.codeforiati.org/api/1/json/en/Version.json" -O codelists/1/Version.json
+wget "https://codelists.codeforiati.org/api/1/json/en/ActivityStatus.json" -O codelists/1/ActivityStatus.json
+wget "https://codelists.codeforiati.org/api/1/json/en/Currency.json" -O codelists/1/Currency.json
+wget "https://codelists.codeforiati.org/api/1/json/en/Sector.json" -O codelists/1/Sector.json
+wget "https://codelists.codeforiati.org/api/1/json/en/SectorCategory.json" -O codelists/1/SectorCategory.json
+wget "https://codelists.codeforiati.org/api/1/json/en/DocumentCategory.json" -O codelists/1/DocumentCategory.json
+wget "https://codelists.codeforiati.org/api/1/json/en/AidType.json" -O codelists/1/AidType.json
+wget "https://codelists.codeforiati.org/api/1/json/en/BudgetNotProvided.json" -O codelists/1/BudgetNotProvided.json
 
 mkdir -p codelists/2
 wget "https://codelists.codeforiati.org/api/json/en/Version.json" -O codelists/2/Version.json
